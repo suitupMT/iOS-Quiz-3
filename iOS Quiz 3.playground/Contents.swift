2a0f225c93bbe4e16eaf32521ea19972371c0679
Travis Long <tlong@mozilla.com>
2019-02-18 20:44:36 -0600

    Push quiz 3

---
+++ iOS Quiz 3.playground/Contents.swift
@@ -0,0 +1,64 @@
+/*:
+ # iOS Development
+ ## Quiz 3
+ 
+ ### Instructions
+ * Enter your answer **BELOW** each question.
+ * Make sure you answer **EVERY** question, some later questions may require the results from previous questions.
+ * Read each question carefully, and answer the best you can.
+ * For questions that require code, yours **MUST** compile.  If you can't get it to work, put it in a comment block and you may get partial credit.
+ * For questions that are text answers, put your answers in comments (single line or block).
+ * When you are complete, save a copy of this playground named with **YOUR NAME**.
+ * Email your quiz to me by the end of class
+ * If you have any questions about the instructions or the test, please ask me for assistance.
+ */
+// LEAVE THIS ALONE (You may need the next couple of lines for your code to work)
+import UIKit
+import Foundation
+// LEAVE THIS ALONE
+//: ### Start Here
+//: Type your **first** and **last** name below as a comment
+
+
+//: ### Question 1
+//: Declare a function that takes an Int as an argument and returns the String equivalent
+
+
+//: ### Question 2
+//: Declare a function that takes two Int parameters and returns the value of the two Ints added together *as a String*
+
+
+//: ### Question 3
+//: What is the definition of a Closure in Swift
+
+
+//: ### Question 4
+//: Create a list of strings and sort them in ascending order by passing a Closure into the .sorted method
+
+
+//: ### Question 5
+//: Explain how Closures *capture* values and why you shouldn't *capture* instances of class objects
+
+
+//: ### Question 6
+//: Declare an enumeration that includes all of the classes you are currently taking (add a couple if you only have one...)
+
+
+//: ### Question 7
+//: Declare a class called Student which has name, major, and GPA properties...  use appropriate data types for the properties
+
+
+//: ### Question 8
+//: Declare an instance of your class from Question 7 and assign it some values for its properties
+
+
+//: ### Question 9
+//: Describe whether classes, structs, and enums are value types or reference types and explain the difference.
+
+
+//: ### Question 10
+//: Why would you choose to use a struct over a class?  Be as specific as possible please.
+
+//: ### Bonus Question
+//: Create a class that inherits from the class in Question 7 and add an array to hold grades, and a function that calculates a GPA from the array
+
